e47c03a2f844a33a2b0ddccc74f37c098721fcf2
Connor Lurring <connorlurring@gmail.com>
2021-08-18 23:56:49 +0100

    fix: bad bracket pair

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ Toolkit.run(async (tools) => {
   }
 
   const tagPrefix = process.env['INPUT_TAG-PREFIX'] || '';
-  const messages = event.commits ? event.commits.map((commit) => commit.message + '\n' + commit.body) : [];
+  const messages = event.commits ? event.commits.map((commit) => commit.message) : [];
 
   const commitMessage = process.env['INPUT_COMMIT-MESSAGE'] || 'ci: version bump to {{version}}';
   console.log('commit messages:', messages);
   const commitMessageRegex = new RegExp(commitMessage.replace(/{{version}}/g, `${tagPrefix}\\d+\\.\\d+\\.\\d+`), 'ig');
-  const isVersionBump = messages.find((message) => commitMessageRegex.test(message)) !== undefined && message.split('-').length === 1;
+  const isVersionBump = messages.find((message) => (commitMessageRegex.test(message) !== undefined && message.split('-').length === 1));
 
   if (isVersionBump) {
     tools.exit.success('No action necessary because we found a previous bump!');
